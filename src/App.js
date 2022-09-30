df9fe479b9baa79baf6219a2e68e39cd9db66bf1
taejin-chang <seniorboy@naver.com>
2022-09-30 17:41:05 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's make OASIS
         </a>
       </header>
     </div>
