1fc74816cf3d07a32cba7e46eac2566d7d7c4f96
taejin-chang <seniorboy@naver.com>
2022-10-11 23:55:50 +0900

    add Signin ui, oasisLogo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import MyPage from "./components/myPage/containers/MyPage";
+import SignIn from "./components/myPage/components/Signin";
+
 function App() {
-  return <div></div>;
+  return(
+  <div>
+    <SignIn/>
+    {/*<MyPage/>*/}
+  </div>);
 }
 
 export default App;
